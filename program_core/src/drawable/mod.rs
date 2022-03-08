2fc1a3f7f06d6a41381195b952cb838d0774c2e8
Kareem Hassanein <coderex24@protonmail.com>
2022-03-08 21:14:54 +0000

    added some definitions in Draw trait

--- program_core/src/drawable/mod.rs
+++ program_core/src/drawable/mod.rs
@@ -1,9 +1,17 @@
-//! #drawable
+//! # drawable
 //! this module will contain code for drawable objects
 //! like lines, circles, rectangles, etc.
-//!
 
-pub trait Draw {}
+use std::collections::HashMap;
+
+/// # Draw
+/// A trait for drawable objects.
+/// this trait will contain methods that helps
+/// construct an SVG tag for the drawable object
+pub trait Draw {
+    fn get_svg_tag_name() -> String;
+    fn get_svg_tag_propeties(self: &Self) -> HashMap<String, String>;
+}
 
 pub mod line2d;
 pub mod point2d;
