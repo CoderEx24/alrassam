395cc9b4b05702dbb1584491dd587fbfc693754f
CoderEx24 <coderex24@protonmail.com>
2022-04-02 17:03:20 +0200

    exported more definitions

--- program_core/src/lib.rs
+++ program_core/src/lib.rs
@@ -8,5 +8,6 @@ pub enum Drawable {
 
 pub use drawable::{
     line2d::Line2D as Line, point2d::Point2D as Point,
-    circle::Circle, text::Text 
+    circle::Circle, text::Text, Color, color_from_hex,
+    RED, GREEN, BLUE, BLACK, WHITE,
 };
