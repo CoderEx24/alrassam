87672a868db003f05fcdca139ca0d8a6197a8c72
CoderEx24 <coderex24@protonmail.com>
2022-04-02 17:53:49 +0200

    added new definition to Draw

--- program_core/src/drawable/mod.rs
+++ program_core/src/drawable/mod.rs
@@ -15,6 +15,8 @@ pub trait Draw {
     fn rotate(self: &mut Self, angle: f64) -> &mut Self;
     fn scale(self: &mut Self, c: f64) -> &mut Self;
 
+    fn contains(self: &Self, point: point2d::Point2D) -> bool;
+
     fn get_svg_tag_name(self: &Self) -> String;
     fn get_svg_tag_properties(self: &Self) -> HashMap<String, String>;
     fn get_svg_inner_content(self: &Self) -> Option<String>;
@@ -25,11 +27,15 @@ pub trait Draw {
         for (key, val) in self.get_svg_tag_properties().iter() {
             svg_tag += format!(" {}={}", key, val).as_str();
         }
-        
+
         // TODO: there must be a better way to do this >:(
         match self.get_svg_inner_content() {
-            Some(txt) => { svg_tag += format!("> {} <{}/>", txt, self.get_svg_tag_name()).as_str(); }
-            None      => { svg_tag += "/>"; }
+            Some(txt) => {
+                svg_tag += format!("> {} <{}/>", txt, self.get_svg_tag_name()).as_str();
+            }
+            None => {
+                svg_tag += "/>";
+            }
         }
 
         svg_tag
@@ -59,7 +65,7 @@ impl ToString for Color {
 /// let red_number   = 0xff0000;
 /// let green_number = 0x00ff00;
 /// let blue_number  = 0x0000ff;
-/// 
+///
 /// assert_eq!(Color(255, 0  , 0  , 0.0), color_from_hex(red_number, 0.0));
 /// assert_eq!(Color(0  , 255, 0  , 0.5), color_from_hex(green_number, 0.5));
 /// assert_eq!(Color(0  , 0  , 255, 0.7), color_from_hex(blue_number, 0.7));
@@ -69,19 +75,18 @@ pub fn color_from_hex(hexnumber: u64, alpha: f32) -> Color {
         ((hexnumber & 0xff0000) >> 16) as u8,
         ((hexnumber & 0x00ff00) >> 8) as u8,
         (hexnumber & 0x0000ff) as u8,
-        alpha
+        alpha,
     )
 }
 
-pub const RED:   Color = Color(255, 0, 0, 1.0);
+pub const RED: Color = Color(255, 0, 0, 1.0);
 pub const GREEN: Color = Color(0, 255, 0, 1.0);
-pub const BLUE:  Color = Color(0, 0, 255, 1.0);
+pub const BLUE: Color = Color(0, 0, 255, 1.0);
 pub const BLACK: Color = Color(255, 255, 255, 1.0);
 pub const WHITE: Color = Color(0, 0, 0, 1.0);
 
+pub mod circle;
 pub mod line2d;
 pub mod point2d;
-pub mod circle;
 pub mod text;
 //pub mod rect2d;
-
