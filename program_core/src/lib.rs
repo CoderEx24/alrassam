a37d88bc4ce32409eddd1f515c69bade52fcd5af
CoderEx24 <coderex24@protonmail.com>
2022-04-03 02:31:19 +0200

    exported the new Vector2

--- program_core/src/lib.rs
+++ program_core/src/lib.rs
@@ -2,11 +2,10 @@ mod drawable;
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum Drawable {
-    Point(drawable::point2d::Point2D),
     Line(drawable::line2d::Line2D),
 }
 
 pub use drawable::{
-    circle::Circle, color_from_hex, line2d::Line2D as Line, point2d::Point2D as Point, text::Text,
+    circle::Circle, color_from_hex, line2d::Line2D as Line, vector::Vector2, text::Text,
     Color, BLACK, BLUE, GREEN, RED, WHITE,
 };
